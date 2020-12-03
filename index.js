105c503332e83df54675026cc45dba8353691f72
Armymn6549 <72281712+Armymn6549@users.noreply.github.com>
2020-12-03 02:02:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new discord.Client();
 const ytdl = require('ytdl-core');
 
 client.on('ready', async () => {
-  client.user.setActivity(STATUS + " 😎")
+  client.user.setActivity(STATUS + " 24/7 Lofi | Quix#0004")
   let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
 
   if(!channel) return;
